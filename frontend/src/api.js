0f1f2b21dd825dae715019759daaa33ffea89979
Keerthana R <kitcbe.25.21bcs022@gmail.com>
2025-08-27 13:09:21 +0530

    env added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+});
 
 export const getTasks = () => API.get("/tasks");
 export const createTask = (task) => API.post("/tasks", task);
